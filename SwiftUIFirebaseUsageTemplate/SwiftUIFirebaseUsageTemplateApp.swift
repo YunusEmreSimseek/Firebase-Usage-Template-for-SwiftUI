a93270dc5612a8828d679979165f9a52811951d2
YunusEmreSimseek <yemresimseek@gmail.com>
2024-08-16 11:47:42 +0300

    Initial Commit

---
+++ SwiftUIFirebaseUsageTemplate/SwiftUIFirebaseUsageTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseUsageTemplateApp.swift
+//  SwiftUIFirebaseUsageTemplate
+//
+//  Created by Emre Simsek on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseUsageTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
